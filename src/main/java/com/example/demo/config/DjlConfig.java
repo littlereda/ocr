d407b28844ff9fb93053206c9232bf6a4548a365
hbc <1761790883@qq.com>
2023-04-23 11:24:24 +0800

    first commit

---
+++ src/main/java/com/example/demo/config/DjlConfig.java
@@ -0,0 +1,72 @@
+package com.example.demo.config;
+
+import ai.djl.modality.Classifications;
+import ai.djl.modality.cv.Image;
+import ai.djl.modality.cv.output.DetectedObjects;
+import ai.djl.paddlepaddle.zoo.cv.imageclassification.PpWordRotateTranslator;
+import ai.djl.paddlepaddle.zoo.cv.objectdetection.PpWordDetectionTranslator;
+import ai.djl.paddlepaddle.zoo.cv.wordrecognition.PpWordRecognitionTranslator;
+import ai.djl.repository.zoo.Criteria;
+import ai.djl.repository.zoo.ModelZoo;
+import ai.djl.repository.zoo.ZooModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Paths;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Configuration
+public class DjlConfig {
+
+
+	/**
+	 * Word detection model
+	 * @return
+	 * @throws Exception
+	 */
+    @Bean
+    public ZooModel<Image, DetectedObjects> detectionModel() throws Exception {
+    	Criteria<Image, DetectedObjects> criteria1 = Criteria.builder()
+                .optEngine("PaddlePaddle")
+                .setTypes(Image.class, DetectedObjects.class)
+                //加载本地模型
+                .optModelPath(Paths.get("D:\\work\\open-anpr\\DJL+PaddleORC\\ch_PP-OCRv3_det_infer.tar"))
+//                .optModelUrls("https://resources.djl.ai/test-models/paddleOCR/mobile/det_db.zip")
+                .optTranslator(new PpWordDetectionTranslator(new ConcurrentHashMap<String, String>()))
+                .build();
+		return ModelZoo.loadModel(criteria1);
+    }
+    /**
+     * Word Direction model
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public ZooModel<Image, Classifications> directionModel() throws Exception {
+    	Criteria<Image, Classifications> criteria1 = Criteria.builder()
+                .optEngine("PaddlePaddle")
+                .setTypes(Image.class, Classifications.class)
+//                .optModelUrls("https://resources.djl.ai/test-models/paddleOCR/mobile/cls.zip")
+                .optModelPath(Paths.get("D:\\work\\open-anpr\\DJL+PaddleORC\\ch_ppocr_mobile_v2.0_cls_infer.tar"))
+                .optTranslator(new PpWordRotateTranslator())
+                .build();
+		return ModelZoo.loadModel(criteria1);
+    }
+    /**
+     * Word Recgonition model
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public ZooModel<Image, String> recgonitionModel() throws Exception {
+    	Criteria<Image, String> criteria1 = Criteria.builder()
+                .optEngine("PaddlePaddle")
+                .setTypes(Image.class, String.class)
+//                .optModelUrls("https://resources.djl.ai/test-models/paddleOCR/mobile/rec_crnn.zip")
+                .optModelPath(Paths.get("D:\\work\\open-anpr\\DJL+PaddleORC\\ch_PP-OCRv3_rec_infer.tar"))
+                .optTranslator(new PpWordRecognitionTranslator())
+                .build();
+    	return ModelZoo.loadModel(criteria1);
+    }
+
+}
---
+++ src/test/java/com/example/demo/service/OrcServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+/**
+ * @author hbc
+ * @version 1.0
+ * @description 描述
+ * @date 2023/4/23 11:13
+ */
+@SpringBootTest
+class OrcServiceTest {
+
+    @Autowired
+    private OrcService orcService;
+
+    @Test
+    void test(){
+        File temp = new File("C:\\Users\\csht\\Desktop\\images\\temp2.png");
+        String code = orcService.orc(temp.getAbsolutePath());
+        System.out.println(code);
+    }
+}
